528d7f6a4f227f913b138a2f601f9a5aada9966a
Brent Anthony R. Tudas <brent.tudas@gmail.com>
2018-02-13 16:30:40 +0800

    Buzzer Initial

--- sketch_feb13a/sketch_feb13a.ino
+++ sketch_feb13a/sketch_feb13a.ino
@@ -1,3 +1,4 @@
+//Keyboard Component Setup
 #include <Keypad.h>
 
 const int keypadRows = 4;
@@ -15,10 +16,33 @@ byte keypadColPins[keypadCols] = {2, 8, 9};
 
 Keypad kpd = Keypad(makeKeymap(keyMap), keypadRowPins, keypadColPins, keypadRows, keypadCols);
 
+//Buzzer Component Setup
+int buzzer = 11;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
 
+  pinMode(buzzer, OUTPUT);
+
+}
+
+void buzz() { 
+  unsigned char i;
+  while(1) {
+    for(i=0;i<80;i++) {
+      digitalWrite(buzzer, HIGH);
+      delay(1);
+      digitalWrite(buzzer, LOW);
+      delay(1);
+    }
+    for(i=0;i<100;i++) {
+      digitalWrite(buzzer, HIGH);
+      delay(2);
+      digitalWrite(buzzer, LOW);
+      delay(2);
+    }
+  }
 }
 
 void loop() {
@@ -27,4 +51,5 @@ void loop() {
   if (keypressed != NO_KEY) {
     Serial.print(keypressed);
   }
+  buzz();
 }
