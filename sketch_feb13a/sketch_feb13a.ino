354f2538fe58e5fb554319d90c82aba30fdb4922
Brent Anthony R. Tudas <brent.tudas@gmail.com>
2018-02-14 06:01:51 +0800

    Latest

--- sketch_feb13a/sketch_feb13a.ino
+++ sketch_feb13a/sketch_feb13a.ino
@@ -1,5 +1,10 @@
 //Keyboard Component Setup
 #include <Keypad.h>
+#include <SoftwareSerial.h>
+
+#define DEBUG true
+
+SoftwareSerial esp8266(0, 1);
 
 const int keypadRows = 4;
 const int keypadCols = 3;
@@ -18,38 +23,115 @@ Keypad kpd = Keypad(makeKeymap(keyMap), keypadRowPins, keypadColPins, keypadRows
 
 //Buzzer Component Setup
 int buzzer = 11;
+// Setting Buzzer mode to False
+boolean buzzerMode = false;
+const int openButtonPin = 12;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  esp8266.begin(9600);
 
   pinMode(buzzer, OUTPUT);
+  pinMode(openButtonPin, INPUT);
 
+
+  sendData("AT+RST\r\n", 2000, DEBUG); // reset module
+  sendData("AT+CWMODE=2\r\n", 1000, DEBUG); // configure as access point
+  sendData("AT+CIFSR\r\n", 1000, DEBUG); // get ip address
+  sendData("AT+CIPMUX=1\r\n", 1000, DEBUG); // configure for multiple connections
+  sendData("AT+CIPSERVER=1,80\r\n", 1000, DEBUG); // turn on server on port 80
 }
 
-void buzz() { 
+void buzz() {
   unsigned char i;
-  while(1) {
-    for(i=0;i<80;i++) {
+  int counter = 0;
+  while (counter <= 3 || buzzerMode == true) {
+    for (i = 0; i < 3; i++) {
       digitalWrite(buzzer, HIGH);
-      delay(1);
+      delay(1000);
       digitalWrite(buzzer, LOW);
-      delay(1);
+      delay(1000);
     }
-    for(i=0;i<100;i++) {
-      digitalWrite(buzzer, HIGH);
-      delay(2);
-      digitalWrite(buzzer, LOW);
-      delay(2);
+    //    tone(buzzer, 1000);
+    counter++;
+    Serial.print(counter);
+  }
+}
+
+String sendData(const char *command, const int timeout, boolean debug) {
+  char response[] = "";
+
+  esp8266.print(command); // send the read character to the esp8266
+
+  long int time = millis();
+
+  while ( (time + timeout) > millis())
+  {
+    while (esp8266.available())
+    {
+
+      // The esp has data so display its output to the serial window
+      char c = esp8266.read(); // read the next character.
+      //        response+=c;
+//      strcpy(response, c);
     }
+  }
+
+  if (debug)
+  {
+    Serial.print(response);
+  }
+
+  return response;
+}
+
+void sendToWebsite() {
+  if (esp8266.available()) {
+
+    //    if(esp8266.find("+IPD,")) {
+    delay(1000);
+    int connectionId = esp8266.read() - 48;
+    char message[] = "<h1> HELLO WORLD FROM ARDUINO </h1>";
+    char cipSend[] = "AT+CIPSEND=";
+
+    //      cipSend += connectionId;
+    //      cipSend += ",";
+    //      cipSend += message.length();
+    //      cipSend += "\r\n";
+
+    sendData(cipSend, 1000, DEBUG);
+    sendData(message, 1000, DEBUG);
+
+
+    char closeCommand[] = "AT+CIPCLOSE=";
+    //      closeCommand += connectionId;
+    //      closeCommand += "\r\n";
+
+    sendData(closeCommand, 3000, DEBUG);
+    Serial.print("Executed send to website.");
+    //    }
+
   }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+//  sendToWebsite();
   char keypressed = kpd.getKey();
+
+  int openButtonStatus = digitalRead(openButtonPin);
+  if (openButtonStatus == HIGH) {
+    Serial.print("HAHAHA");
+    buzzerMode = !buzzerMode;
+    Serial.print(buzzerMode);
+  }
+
   if (keypressed != NO_KEY) {
     Serial.print(keypressed);
+    buzzerMode = true;
+    buzz();
   }
-  buzz();
+
+  //  buzz();
 }
